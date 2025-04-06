9d68a9bf7821b1b59079e0a776362974ee465b5a
Karan Modh <72705736+karan-modh-5@users.noreply.github.com>
2025-04-06 12:39:32 +0530

    Update kashword.py
    -v1.9.3

--- kashword.py
+++ kashword.py
@@ -1,33 +1,37 @@
-import time
-import hashlib
-import sys
-import getpass
-import random
-import string
-import math
-import argparse
-import shutil  
+import time, hashlib, sys, getpass, random, string, math, argparse, shutil, os
 
 # Set these parameters:
+## Unique user input parameters:
 length = 15 # Please Define Lenght Here
 start_from = 1 # Plaese Set Start From Here
 algorithm = "SHA256" # Please make sure that entered algorithm is available in hashlib.
-version = "1.9.0"
-loading_time = 0.005 # Time delay for the loading effect
-terminal_width, _ = shutil.get_terminal_size()
+plaintext_prefix = ""
+plaintext_suffix = ""
+after_hash_masking_prefix = ""
+after_hash_masking_suffix = ""
+
+## Script parameters:
+version = "1.9.3"
 password_masking = "*"
+loading_time = 0.005 # Time delay for the loading effect
+
+os_name = os.name
+kashword_printed_on_terminal = False
+terminal_width, _ = shutil.get_terminal_size()
+after_hash_masking_prefix_lenth = len(after_hash_masking_prefix)
+after_hash_masking_suffix_lenth = len(after_hash_masking_suffix)
 
 # Variables for storing input parameters
 clearpass = ""
-kashword = ""
+selected_length_hash_output = ""
 
 parser = argparse.ArgumentParser(description="Kashword")
 parser.add_argument("-v", action="store_true", help="Print version info")
 parser.add_argument("-l", type=int, help="Specify length")
-parser.add_argument("-c", help="Specify Cypher")
-parser.add_argument("-C", action='store_true', help="Cypher Will Entered In Clear Text")
-parser.add_argument("-P", action='store_true', help="Hide Lenght")
 parser.add_argument("-s", type=int, help="Specify starting point")
+parser.add_argument("-C", action='store_true', help="Cypher Will Entered In Clear Text")
+parser.add_argument("-R", action='store_true', help="Skip After Hash Maasking")
+parser.add_argument("-P", action='store_true', help="Hide Lenght")
 
 args = parser.parse_args()
 
@@ -37,9 +41,6 @@ if args.v:
 
 if args.l:
     length = args.l
-
-if args.c:
-    clearpass = args.c
 
 if args.C:
     clearpass = "clear"
@@ -64,67 +65,20 @@ def progress_bar(progress, total):
     print(f"\rKashword |{bar}| {percent:.2f}%", end="\r")
 
 # ASCII logo used in the script
-roman_logo = [
-    ".-------------------------------------------------------------------------------------------.",
-    "| oooo    oooo                    oooo                                                  .o8 |",
-    "| `888   .8P'                     `888                                                 \"888 |",
-    "|  888  d8'     .oooo.    .oooo.o  888 .oo.   oooo oooo    ooo  .ooooo.  oooo d8b  .oooo888 |",
-    "|  88888[      `P  )88b  d88(  \"8  888P\"Y88b   `88. `88.  .8'  d88' `88b `888\"\"8P d88' `888 |",
-    "|  888`88b.     .oP\"888  `\"Y88b.   888   888    `88..]88..8'   888   888  888     888   888 |",
-    "|  888  `88b.  d8(  888  o.  )88b  888   888     `888'`888'    888   888  888     888   888 |",
-    "| o888o  o888o `Y888\"\"8o 8\"\"888P' o888o o888o     `8'  `8'     `Y8bod8P' d888b    `Y8bod88P |",
-    "`-------------------------------------------------------------------------------------------'"
-]
-
-thick_logo = [
-    ".--------------------------------------------------.",
-    "| 8  dP           8                              8 |",
-    "| 8wdP  .d88 d88b 8d8b. Yb  db  dP .d8b. 8d8b .d88 |",
-    "| 88Yb  8  8 `Yb. 8P Y8  YbdPYbdP  8' .8 8P   8  8 |",
-    "| 8  Yb `Y88 Y88P 8   8   YP  YP   `Y8P' 8    `Y88 |",
-    "`--------------------------------------------------'"
-]
-
-just_K_logo = [
-    ".--------------.",
-    "| oooo    oooo |",
-    "| `888   .8P'  |",
-    "|  888  d8'    |",
-    "|  88888[      |",
-    "|  888`88b.    |",
-    "|  888  `88b.  |",
-    "| o888o  o888o |",
-    "`--------------'"
-]
-
-RubiFont_logo = [
-#    ".------------------------------------------.",
-    " ▗▖ ▗▖ ▗▄▖  ▗▄▄▖▗▖ ▗▖▗▖ ▗▖ ▗▄▖ ▗▄▄▖ ▗▄▄▄  ",
-    " ▐▌▗▞▘▐▌ ▐▌▐▌   ▐▌ ▐▌▐▌ ▐▌▐▌ ▐▌▐▌ ▐▌▐▌  █ ",
-    " ▐▛▚▖ ▐▛▀▜▌ ▝▀▚▖▐▛▀▜▌▐▌ ▐▌▐▌ ▐▌▐▛▀▚▖▐▌  █ ",
-    " ▐▌ ▐▌▐▌ ▐▌▗▄▄▞▘▐▌ ▐▌▐▙█▟▌▝▚▄▞▘▐▌ ▐▌▐▙▄▄▀ ",
-#    "`------------------------------------------'"
-]
-
 tmplr_logo = [
     "┓┏┓   ┓         ┓",
     "┃┫ ┏┓┏┣┓┓┏┏┏┓┏┓┏┫",
     "┛┗┛┗┻┛┛┗┗┻┛┗┛┛ ┗┻"
 ]
 # Function to display the loading logo
-def slow_print_logo(RubiFont_logo, delay):
-    if int(terminal_width) > 136:
-        for line in RubiFont_logo:
-            print(line)
-            time.sleep(delay)  # Add delay between each line
-    if int(terminal_width) < 135:
-        for line in tmplr_logo:
-            print(line)
-            time.sleep(delay)  # Add delay between each line
+def slow_print_logo(delay):
+    for line in tmplr_logo:
+        print(line)
+        time.sleep(delay)  # Add delay between each line
 
 def logo_loading():
     print()
-    slow_print_logo(RubiFont_logo, delay=loading_time+0.01)  # Adjust delay for slower/faster printing
+    slow_print_logo(delay=loading_time+0.01)  # Adjust delay for slower/faster printing
     print()
 
 # Function to clear the terminal screen
@@ -182,11 +136,12 @@ try:
 
     # Hash the entered password 
     hash = hashlib.new(algorithm)
+    clearpass = plaintext_prefix + clearpass + plaintext_suffix
     hash.update(clearpass.encode())
     hash_output = hash.hexdigest()
 
     # Extract the substring from the hash based on the specified length and starting point
-    kashword = hash_output[start_from-1:start_from-1 + length]
+    selected_length_hash_output = hash_output[start_from-1:start_from-1 + length]
 
     # Print random samples of characters from the defined strings
     slow_print(''.join(random.sample(s, 1000)))
@@ -196,22 +151,52 @@ try:
 
     total_steps = 100
     progress_bar(0, total_steps)
-    
+
     # Simulate a task that takes time (e.g., reading files, processing data, etc.)
     for i in range(total_steps):
         time.sleep(loading_time)  # Simulating work (replace with real task)
         progress_bar(i + 1, total_steps)
     print()  # Print a newline after completion
 
-except KeyboardInterrupt:
-    # Handle the interruption (optional)
-    print("\nKashword Stop Executing.")
-    clearpass = None  # Clear password from memory
-finally:
     print("\n[Kashword_v{}] (Kashword will be erased in 5 seconds)".format(version))
+    if args.R:
+        kashword = selected_length_hash_output
+    else:
+        kashword = (after_hash_masking_prefix + selected_length_hash_output[after_hash_masking_prefix_lenth:])[:len(selected_length_hash_output) - after_hash_masking_suffix_lenth] + after_hash_masking_suffix
     print(f"\r{kashword}", end="")
+    kashword_printed_on_terminal = True
+    if os_name == 'nt':
+        command = f'echo {kashword.rstrip()}| clip'
+        os.system(command)
     time.sleep(5)
-    remove = password_masking * len(kashword)  # or len(remove) if using a pattern
+    remove = password_masking * length  # or len(remove) if using a pattern
     print(f"\r{remove}", end="\n")
+    kashword_printed_on_terminal = False
+
+except KeyboardInterrupt:
+    #Handle the interruption (optional)
+    if kashword_printed_on_terminal == True:
+        remove = password_masking * length  # or len(remove) if using a pattern
+        print(f"\r{remove}", end="\n")
+        kashword_printed_on_terminal = False
+
+    print("\nKashword Stop Executing.")
+
+finally:
     clearpass = None  # Clear password from memory
     kashword = None
+    selected_length_hash_output = None
+    command = None
+    start_from = None
+    algorithm = None
+    plaintext_prefix = None
+    plaintext_suffix = None
+    after_hash_masking_prefix = None
+    after_hash_masking_suffix = None
+    after_hash_masking_prefix_lenth = None
+    after_hash_masking_suffix_lenth = None
+    if kashword_printed_on_terminal == True:
+        remove = password_masking * length  # or len(remove) if using a pattern
+        print(f"\r{remove}", end="\n")
+    length = None
+    kashword_printed_on_terminal = None
